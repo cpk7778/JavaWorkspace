ed2e251ae4a7aee92e4057c16361c4a39e40c0b2
Administrator <cpk7778@gmail.com>
2022-12-20 22:45:20 +0900

    If 추가

--- src/chap_04/_01_if.java
+++ src/chap_04/_01_if.java
@@ -9,14 +9,24 @@ public class _01_if {
             System.out.println("아이스 아메리카노 +1");
             System.out.println("샷 추가");
         }
-        System.out.println("커피 주문 완료");
+        System.out.println("커피 주문 완료 #1");
 
         // 커피 두 잔 마실 경우 + 14시 이전인지 확인
         hour = 10; // 오전 10시
-        boolean morningCoffee = true; // 모닝 커피
-        if (hour < 14 && morningCoffee == false){
+        boolean morningCoffee = false; // 모닝 커피
+//        if (hour < 14 && morningCoffee == false){
+        if (hour < 14 && !morningCoffee){ // And 연산자 && 추가 // boolean 앞에 ! 추가 -> 부정
             System.out.println("아이스 아메리카노 +1");
         }
-        System.out.println("커피 주문 완료");
+        System.out.println("커피 주문 완료 #2");
+
+        // 오후 2시 이후 또는 모닝 커피를 마신 경우
+        hour = 15;
+        morningCoffee = true;
+//        if (hour >= 14 || morningCoffee == true) { // or 연산자 || 추가 // boolean 그냥 입력 긍정
+        if (hour >= 14 || morningCoffee) { // or 연산자 || 추가 // boolean 그냥 입력 긍정
+            System.out.println("아이스 아메리카노 +1");
+        }
+        System.out.println("커피 주문 완료 #3");
     }
 }
