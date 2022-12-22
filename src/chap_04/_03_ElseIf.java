aca2b8d45d028595d65cd1b668eb047f8b67da0e
cpk78 <cpk7778@gmail.com>
2022-12-22 22:58:55 +0900

    Commit 3

---
+++ src/chap_04/_03_ElseIf.java
@@ -0,0 +1,52 @@
+package chap_04;
+
+public class _03_ElseIf {
+    public static void main(String[] args) {
+        // 조건문 Else if
+        // 한라봉 에이드가 있으면 +1
+        // 또는 망고 주스가 있으면 +1
+        // 또는  아이스 아메리카노 +1
+
+        boolean hallaBongAde = true;
+        boolean mangoJuice = true;
+
+        if (hallaBongAde) {
+            System.out.println("한라봉 에이드 +1");
+        } else if (mangoJuice) {
+            System.out.println("망고 주스 +1");
+        } else {
+            System.out.println("아이스 아메리카노 +1");
+        }
+        System.out.println("커피 주문 완료 #1\n");
+
+        // else if는 여러번 사용 가능
+        hallaBongAde = false;
+        mangoJuice = false;
+        boolean orangeJuice = true;
+
+        if (hallaBongAde) {
+            System.out.println("한라봉 에이드 +1");
+        } else if (mangoJuice) {
+            System.out.println("망고 주스 +1");
+        } else if (orangeJuice) {
+            System.out.println("오랜지 주스 +1");
+        } else {
+            System.out.println("아이스 아메리카노 +1");
+        }
+        System.out.println("커피 주문 완료 #2\n");
+
+        // else는 없어도 가능
+        hallaBongAde = false;
+        mangoJuice = false;
+        orangeJuice = true;
+
+        if (hallaBongAde) {
+            System.out.println("한라봉 에이드 +1");
+        } else if (mangoJuice) {
+            System.out.println("망고 주스 +1");
+        } else if (orangeJuice) {
+            System.out.println("오랜지 주스 +1");
+        }
+        System.out.println("커피 주문 완료 #3\n");
+    }
+}
